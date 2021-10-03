652239ee1774c81a8624a219acdb1165e4747849
UrteMelyte <86527135+UrteMelyte@users.noreply.github.com>
2021-10-02 20:27:41 -0500

    Challenge 2 complete

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -135,24 +135,39 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     if (magnitude > 5) {
       return "#ea822c";
     }
-    return "#98ee00";
+    return "#ee9c00";
   }
   
   // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
-  
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
   
   // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
   // sets the style of the circle, and displays the magnitude and location of the earthquake
   //  after the marker has been created and styled.
-  L.geoJson(, {
-      
-  });
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+   // We create a popup for each circleMarker to display the magnitude and location of the earthquake
+   //  after the marker has been created and styled.
+   onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
   // 8. Add the major earthquakes layer to the map.
-  
-  // 9. Close the braces and parentheses for the major earthquake data.
-  });
+}).addTo(majorEQ);
 
-
+// 9. Close the braces and parentheses for the major earthquake data.
+majorEQ.addTo(map);
+});
 
 
 
